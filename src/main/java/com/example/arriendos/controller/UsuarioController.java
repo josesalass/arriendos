1ed130eedc81f5e344e08a798ebec17227ab14b0
Nicolas Romero <nicolas.romero1701@alumnos.ubiobio.cl>
2022-12-18 21:56:53 -0300

    CRUD usuarios en /usuario/user

--- src/main/java/com/example/arriendos/controller/UsuarioController.java
+++ src/main/java/com/example/arriendos/controller/UsuarioController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,22 +27,57 @@ public class UsuarioController {
 	@Autowired 
 	UsuarioService service;
 	
-	@GetMapping("user")
-	public String getUser(Model model, String rut) {
-		rut = "20077281-4";
-		Usuario user = service.getById(rut);
+	@GetMapping("/user")
+	public String getUser(Model model) {
 		
+		model.addAttribute("usuario", service.getAll());
 		
-		model.addAttribute("usuario",user);
+		return "usuarios";
+	}
+	
+	@GetMapping("/nuevo")
+	public String crearUser(Model modelo) {
+		Usuario usuario = new Usuario();
+		modelo.addAttribute("usuario", usuario);
 		
-		return "Perfil";
+		return "crear_user";
 	}
 	
 	
 	@PostMapping("/save")
-	public String saveUser(@Valid Usuario user, Model model) {
+	public String saveUser(Usuario user, Model model) {
 		service.save(user);
 		
-		return "Perfil";
+		return "redirect:/usuario/user";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String mostarFormEditar(@PathVariable String id, Model modelo, Usuario usuario) {
+		//Usuario user = service.getUserId(rut);
+		//modelo.addAttribute("usuario", user);
+		modelo.addAttribute("usuario", service.getUserId(id));
+		return "editar_user";
+		
+	}
+	@PostMapping("/user/{id}")
+	public String actualizarUser(@PathVariable String id, @ModelAttribute("usuario") Usuario usuario, Model modelo) {
+		Usuario usuarioExistente = service.getUserId(id);
+		usuarioExistente.setId(id);
+		usuarioExistente.setNombre(usuario.getNombre());
+		usuarioExistente.setApellido1(usuario.getApellido1());
+		usuarioExistente.setApellido2(usuario.getApellido2());
+		usuarioExistente.setNacimiento(usuario.getNacimiento());
+		usuarioExistente.setPassword(usuario.getPassword());
+		usuarioExistente.setImg(usuario.getImg());
+		
+		service.actualizarUser(usuarioExistente);
+		return "redirect:/usuario/user";
+		
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminarUser(@PathVariable String id) {
+		service.eliminarEstudiante(id);
+		return "redirect:/usuario/user";
 	}
 }
